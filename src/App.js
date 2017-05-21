89c1eb2d3a58066c1f9b328348c75165c93f4059
wandi34 <info@andreas-winter.eu>
2017-05-21 22:36:21 +0200

    Add first gallery with test pictures

--- src/App.js
+++ src/App.js
@@ -1,21 +1,49 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { render } from 'react-dom';
+import Gallery from 'react-grid-gallery';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      images: images
+    };
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Gallery
+        images={this.state.images}/>
     );
   }
 }
+
+const images = (
+    [{
+            src: "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_b.jpg",
+            thumbnail: "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_n.jpg",
+            thumbnailWidth: 320,
+            thumbnailHeight: 174,
+            isSelected: false,
+            caption: "After Rain (Jeshu John - designerspics.com)"
+    },
+    {
+            src: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg",
+            thumbnail: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_n.jpg",
+            thumbnailWidth: 320,
+            thumbnailHeight: 212,
+            tags: [{value: "Ocean", title: "Ocean"}, {value: "People", title: "People"}],
+            caption: "Boats (Jeshu John - designerspics.com)"
+    },
+
+    {
+            src: "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg",
+            thumbnail: "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_n.jpg",
+            thumbnailWidth: 320,
+            thumbnailHeight: 212
+    }]
+  )
 
 export default App;
