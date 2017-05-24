0bb4b6d56e90dfa64526ce3dbb6f29654227b9bf
wandi34 <info@andreas-winter.eu>
2017-05-24 21:10:21 +0200

    Fix reducer

--- src/reducers/imagesReducer.js
+++ src/reducers/imagesReducer.js
@@ -5,7 +5,7 @@ export const initialState = {
 export default (state = initialState, action) => {
     switch (action.type) {
         case 'GET_IMAGES':
-          return Object.assign(state, {
+          return Object.assign({}, state, {
                 images: ["Eins", "Zwei", "Vier"]
               })
         default:
